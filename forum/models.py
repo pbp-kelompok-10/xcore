224b3772319baa99996825966a3656a04f91d8b9
nayraarr <zita.nayra.ardini@gmail.com>
2025-10-21 17:48:09 +0700

    Update Models Forum

--- forum/models.py
+++ forum/models.py
@@ -21,4 +21,4 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
-        return self.title   
+        return self.id + "-" + self.forum.id
