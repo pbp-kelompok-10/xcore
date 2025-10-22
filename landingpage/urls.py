85da28950980998e8c6482a5d4ba505ea9fb9602
nayraarr <zita.nayra.ardini@gmail.com>
2025-10-22 15:17:00 +0700

    Add Login Page

--- landingpage/urls.py
+++ landingpage/urls.py
@@ -7,4 +7,5 @@ app_name = 'landingpage'
 urlpatterns = [
     path('', views.landing_home, name='home'),
     path('register/', views.register, name='register'),
+    path('login/', views.login_user, name='login'),
 ]
--- landingpage/views.py
+++ landingpage/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
 from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def landing_home(request):
@@ -25,8 +26,22 @@ def register(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Your account has been successfully created!')
-            return redirect('landingpage:home')
+            return redirect('landingpage:login')
     context = {'form':form}
     return render(request, 'register.html', context)
+
+def login_user(request):
+   if request.method == 'POST':
+      form = AuthenticationForm(data=request.POST)
+
+      if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('landingpage:home')
+
+   else:
+      form = AuthenticationForm(request)
+   context = {'form': form}
+   return render(request, 'login.html', context)
 
 
