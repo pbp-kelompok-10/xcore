66e6b4e4ad83d5f7d32f9f306faeb9c5e40244b4
alvinchristian <alvch6@gmail.com>
2025-10-23 18:29:57 +0700

    add logout

--- landingpage/urls.py
+++ landingpage/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.landing_home, name='home'),
     path('register/', views.register, name='register'),
     path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
 ]
