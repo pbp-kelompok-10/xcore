2b6c38d0abbc86bb29b0ace912ce88013c7d9e5e
Anak Agung Ngurah Abhivadya Nandana <abhivadya1@gmail.com>
2025-10-24 21:57:06 +0700

    admin.py statistik update(fix error)

--- statistik/admin.py
+++ statistik/admin.py
@@ -3,12 +3,6 @@ from .models import Statistik
 
 @admin.register(Statistik)
 class StatistikAdmin(admin.ModelAdmin):
-    list_display = ['id', 'match_id', 'shoot_home', 'shoot_away', 'ball_possession_home', 'ball_possession_away']
-from django.contrib import admin
-from .models import Statistik
-
-@admin.register(Statistik)
-class StatistikAdmin(admin.ModelAdmin):
     list_display = (
         'match',
         'shoot_home', 'shoot_away',
