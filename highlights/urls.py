51090e339b5d4c9af3abb7a8cc450ae29154a5d0
Anak Agung Ngurah Abhivadya Nandana <abhivadya1@gmail.com>
2025-10-24 23:20:33 +0700

    update comment

--- highlights/urls.py
+++ highlights/urls.py
@@ -5,13 +5,7 @@ app_name = 'highlights'
 
 urlpatterns = [
     path('<uuid:match_id>/', views.highlight_detail, name='match_highlights'),
-    
-    # Create operation
     path('<uuid:match_id>/create', views.highlight_create, name='highlight_create'),
-
-    # Update operation
     path('<uuid:match_id>/update/', views.highlight_update, name='highlight_update'),
-
-    # Delete operation
     path('<uuid:match_id>/delete/', views.highlight_delete, name='highlight_delete'),
 ]
