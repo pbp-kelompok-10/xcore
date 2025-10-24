b86529cba126dac78d3eb4428e1f7b9bb2873c8a
Anak Agung Ngurah Abhivadya Nandana <abhivadya1@gmail.com>
2025-10-24 21:15:11 +0700

    update admin scoreboard

--- scoreboard/admin.py
+++ scoreboard/admin.py
@@ -1,4 +1,9 @@
-# from django.contrib import admin
-# from .models import Match  
+from django.contrib import admin
+from .models import Match
 
-# admin.site.register(Match)
+@admin.register(Match)
+class MatchAdmin(admin.ModelAdmin):
+    list_display = ['home_team', 'away_team', 'home_score', 'away_score', 'match_date', 'status']
+    list_filter = ['status', 'match_date']
+    search_fields = ['home_team', 'away_team']
+    readonly_fields = ['home_team', 'away_team']
