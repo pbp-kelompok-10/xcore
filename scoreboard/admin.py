7423102d78086ce2fe2a691398bc0f1366866b04
Anak Agung Ngurah Abhivadya Nandana <abhivadya1@gmail.com>
2025-10-24 19:36:28 +0700

    admin

--- scoreboard/admin.py
+++ scoreboard/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
-from .models import Match  
+# from django.contrib import admin
+# from .models import Match  
 
-admin.site.register(Match)
+# admin.site.register(Match)
