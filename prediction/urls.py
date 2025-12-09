70e1a53ff172e98d95cbe9730944028a5cd1b87d
querycard <khansa.dinda@ui.ac.id>
2025-12-09 07:53:12 +0700

    add navigation tab, update, delete, vote, and dialog for prediction

--- prediction/urls.py
+++ prediction/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import prediction_list, submit_vote, my_votes, update_vote, delete_vote, show_json
+from .views import prediction_list, submit_vote, my_votes, update_vote, delete_vote, show_json, submit_vote_flutter, update_vote_flutter, delete_vote_flutter, show_my_votes_json
 
 app_name = 'prediction'
 
@@ -10,4 +10,8 @@ urlpatterns = [
     path('my-votes/', my_votes, name='my_votes'),
     path('my-votes/delete/<uuid:vote_id>/', delete_vote, name='delete_vote'),  
     path('json/', show_json, name='show_json'),
+    path('submit-vote-flutter/', submit_vote_flutter, name='submit_vote_flutter'),
+    path('update-vote-flutter/', update_vote_flutter, name='update_vote_flutter'),
+    path('delete-vote-flutter/', delete_vote_flutter, name='delete_vote_flutter'),
+    path('json-my-votes/', show_my_votes_json, name='show_my_votes_json'),
 ]
