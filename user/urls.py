633c2f472a3cb0e7e900c81fb67d55339ed33202
alvinchristian <alvch6@gmail.com>
2025-12-20 14:35:25 +0700

    add flutter profile pic

--- user/urls.py
+++ user/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'user'
 urlpatterns = [
     path("create-admin/", views.create_admin_user, name="create-admin"),
+    path("api/profile/", views.get_user_profile, name="api-profile"),
 ]
